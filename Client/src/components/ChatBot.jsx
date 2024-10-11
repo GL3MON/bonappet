9b4e0d5c20435a58b2f4f22b15324c7c6e386209
GL3MON <harisshragavarchives@gmail.com>
2024-10-11 13:58:45 +0530

    Added the Chat Page

---
+++ Client/src/components/ChatBot.jsx
@@ -0,0 +1,9 @@
+import '../styles/ChatBot.css';
+
+const ChatBot = () => {
+    return <div className="chatbot-container">
+        <div className="chatbot-overlay" />
+    </div>
+}
+
+export default ChatBot;
